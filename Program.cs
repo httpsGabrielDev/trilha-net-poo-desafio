cd90e28a7adf763bc124d2b45fd5c1dfd2a2f973
Gabriel Fernando <gabrielfernandocarreraps@gmail.com>
2024-01-05 14:27:00 -0300

    DESAFIO POO CONCLUIDO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "111111111", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "2222222", modelo: "Modelo 2", imei: "222222222", memoria: 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
